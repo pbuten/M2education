3e4c57d6d3406f0bde4cfcafdc1222b52678ef46
Pavlo Butenko <pavlo.buten@gmail.com>
2022-06-09 13:05:39 +0300

    First commit

---
+++ bin/m2update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy -f en_US
+php bin/magento indexer:reindex
+php bin/magento cache:clean
+php bin/magento cache:flush
